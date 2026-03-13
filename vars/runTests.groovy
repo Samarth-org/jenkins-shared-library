1f48fd9f10cc71a6df160b753338a03bb4db8362
Samarth DevTools <samarthm@devtools.in>
2026-03-13 16:41:38 +0530

    Enhance runTests.groovy to copy test results
    Added command to copy test results XML to workspace.

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,5 +1,7 @@
 def call() {
-    docker.image('node:20').inside('-u root:root') {  // 👈 add this
+    docker.image('node:20').inside('-u root:root') {
         sh 'npm test'
+        // copy xml out of container to workspace
+        sh 'cp test-results.xml $WORKSPACE/test-results.xml || true'
     }
 }
