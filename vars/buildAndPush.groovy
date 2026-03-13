2c1f1b64206a5d8820a4d8d9d257d7e568aa4538
Samarth DevTools <samarthm@devtools.in>
2026-03-13 11:02:44 +0530

    edited

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,5 +1,4 @@
 def call(String imageName, String tag) {
-    Run directly on Jenkins agent where Docker IS installed
     sh "docker build -t ${imageName}:${tag} ."
     withDockerRegistry(credentialsId: 'docker-hub-credentials', url: '') {
         sh "docker push ${imageName}:${tag}"
